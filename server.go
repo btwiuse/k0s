66227c5cd1f908b1bc8a64029b6686925c20db18
navigaid <navigaid@gmail.com>
2019-05-11 21:42:41 +0800

    server: dump connection pool on client connect/disconnect

--- server.go
+++ server.go
@@ -2,23 +2,66 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+
+	"github.com/google/uuid"
 )
 
+type Client struct {
+	UUID string
+	Conn net.Conn
+	Quit chan struct{}
+}
+
+func NewClient(uuid string, conn net.Conn, quit chan struct{}) *Client {
+	return &Client{
+		UUID: uuid,
+		Conn: conn,
+		Quit: quit,
+	}
+}
+
+type Pool map[string]*Client
+
+var ClientPool = make(Pool)
+
+func (p Pool) Del(uuid string) {
+	delete(p, uuid)
+}
+
+func (p Pool) Add(client *Client) {
+	p[client.UUID] = client
+}
+
+func (p Pool) Dump() {
+	log.Println("[client pool]")
+	for uuid, client := range p {
+		fmt.Println(uuid, client.Conn.RemoteAddr())
+	}
+}
+
 func hijacker(w http.ResponseWriter, r *http.Request) {
+	uuid := uuid.New().String()
 	conn, _, err := w.(http.Hijacker).Hijack()
 	if err != nil {
 		log.Println(nil)
 	}
-	log.Println("connected:", conn.RemoteAddr())
+	log.Println("connected:", uuid, conn.RemoteAddr())
 
 	quit := make(chan struct{})
+	client := NewClient(uuid, conn, quit)
+	ClientPool.Add(client)
+	ClientPool.Dump()
 	copy := func(dst io.Writer, src io.Reader) {
-		defer log.Println("disconnected:", conn.RemoteAddr())
-		defer close(quit)
+		defer ClientPool.Dump()
+		defer log.Println("disconnected:", client.Conn.RemoteAddr())
+		defer close(client.Quit)
+		defer ClientPool.Del(uuid)
 		buf := make([]byte, 1)
 		for {
 			_, err := src.Read(buf)
