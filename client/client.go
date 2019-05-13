9e34d236ae819b78a32f729eff59715097943b70
navigaid <navigaid@gmail.com>
2019-05-13 22:39:56 +0800

    client: enable local build && version info

--- client/client.go
+++ client/client.go
@@ -21,6 +21,22 @@ func run(oneliner string) []byte {
 	return out
 }
 
+var (
+	buildCode  = strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv BUILD_CODE`)))
+	dockerRepo = strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv DOCKER_REPO`)))
+	gitBranch  = strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_BRANCH`)))
+	gitTag     = strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_TAG`)))
+	gitSha1    = strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_SHA1`)))
+	gitMsg     = strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_MSG`)))
+	ip         = strings.TrimSpace(string(run(`curl -sL ip.sb`)))
+	pwd        = strings.TrimSpace(string(run(`pwd`)))
+	whoami     = strings.TrimSpace(string(run(`whoami`)))
+	hostname   = strings.TrimSpace(string(run(`hostname`)))
+	commit     string
+	built      string
+	branch     string
+)
+
 type Header struct {
 	BuildCode  string `json:"build_code"`
 	DockerRepo string `json:"docker_repo"`
@@ -32,6 +48,9 @@ type Header struct {
 	Pwd        string `json:"pwd"`
 	Whoami     string `json:"whoami"`
 	Hostname   string `json:"hostname"`
+	Commit     string `json:"commit"`
+	Built      string `json:"built"`
+	Branch     string `json:"branch"`
 }
 
 func main() {
@@ -49,16 +68,6 @@ func main() {
 	}
 	log.Println("connected:", conn.LocalAddr(), conn.RemoteAddr())
 	time.Sleep(time.Second)
-	buildCode := strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv BUILD_CODE`)))
-	dockerRepo := strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv DOCKER_REPO`)))
-	gitBranch := strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_BRANCH`)))
-	gitTag := strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_TAG`)))
-	gitSha1 := strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_SHA1`)))
-	gitMsg := strings.TrimSpace(string(run(`docker exec $(docker ps --format '{{if (eq (index (split (printf "%s" .Image) ":") 0) "docker/highland_builder")}}{{.ID}}{{end}}' | grep .) printenv GIT_MSG`)))
-	ip := strings.TrimSpace(string(run(`curl -sL ip.sb`)))
-	pwd := strings.TrimSpace(string(run(`pwd`)))
-	whoami := strings.TrimSpace(string(run(`whoami`)))
-	hostname := strings.TrimSpace(string(run(`hostname`)))
 	conn.Write(
 		[]byte(pretty.JSONString(&Header{
 			BuildCode:  buildCode,
@@ -71,6 +80,9 @@ func main() {
 			Pwd:        pwd,
 			Whoami:     whoami,
 			Hostname:   hostname,
+			Commit:     commit,
+			Built:      built,
+			Branch:     branch,
 		})),
 	)
 	scanner := bufio.NewScanner(conn)
