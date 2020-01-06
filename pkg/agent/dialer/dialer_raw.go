01bca54a565650e4bcc42cc8e76016b522cdf156
navigaid <navigaid@gmail.com>
2020-01-06 09:16:33 +0800

    fix sending http request to https server

--- pkg/agent/dialer/dialer_raw.go
+++ pkg/agent/dialer/dialer_raw.go
@@ -3,6 +3,7 @@
 package dialer
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net"
@@ -13,10 +14,18 @@ func (d *dialr) Dial(p string) (conn net.Conn, err error) {
 		c = d.c
 	)
 
-	conn, err = net.Dial("tcp", c.GetAddr())
-	if err != nil {
-		return nil, err
+	switch c.GetScheme() {
+	case "http":
+		conn, err = net.Dial("tcp", c.GetAddr())
+		if err != nil {
+			return nil, err
+		}
+	case "https":
+		conn, err = tls.Dial("tcp", c.GetAddr(), &tls.Config{
+			InsecureSkipVerify: c.GetInsecure(),
+		})
 	}
+
 	_, err = io.WriteString(conn, fmt.Sprintf("GET %s HTTP/1.1\r\nHost: %s\r\nConnection: Keep-Alive\r\n\r\n", p, c.GetHostname()))
 	if err != nil {
 		return nil, err
