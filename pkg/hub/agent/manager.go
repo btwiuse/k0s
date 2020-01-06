fde5e45bde94b3634bbb53c917359db8e6102907
navigaid <navigaid@gmail.com>
2020-01-07 03:27:13 +0800

    remove unsed comment

--- pkg/hub/agent/manager.go
+++ pkg/hub/agent/manager.go
@@ -11,7 +11,6 @@ import (
 
 var (
 	_ hub.SessionManager = (*sessionManager)(nil)
-	// _ hub.RPCManager     = (*rpcManager)(nil)
 )
 
 type sessionManager struct {
@@ -31,61 +30,3 @@ func NewSessionManager() hub.SessionManager {
 		Manager: manager.NewManager(),
 	}
 }
-
-/*
-type rpcManager struct {
-	pkg.Manager
-}
-
-// todo: use iterator
-func (rm *rpcManager) Last() hub.RPC {
-	if rm.Manager.Size() == 0 {
-		return nil
-	}
-	return rm.Values()[rm.Size()-1].(hub.RPC)
-}
-
-// todo: use iterator
-func (rm *rpcManager) AddRPC(rc hub.RPC) {
-	if rm.Size() > 3 {
-		// rm the first
-	}
-	rm.Manager.Add(rc)
-}
-
-func NewRPCManager() hub.RPCManager {
-	return &rpcManager{
-		Manager: manager.NewManager(),
-	}
-}
-
-type rpcc struct {
-	created time.Time
-	id      string
-	name    string
-	*rpc.Client
-}
-
-func (r *rpcc) Time() time.Time {
-	return r.created
-}
-
-func (r *rpcc) Name() string {
-	return r.name
-}
-
-func (r *rpcc) ID() string {
-	return r.id
-}
-
-func ToRPC(name, id string) func(*rpc.Client) hub.RPC {
-	return func(rc *rpc.Client) hub.RPC {
-		return &rpcc{
-			created: time.Now(),
-			id:      id,
-			name:    name,
-			Client:  rc,
-		}
-	}
-}
-*/
