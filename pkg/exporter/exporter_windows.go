8248b64769bcf74741db6651b70d71248457752b
navigaid <navigaid@gmail.com>
2020-02-02 02:18:44 +0800

    windows: refactor metrics

---
+++ pkg/exporter/exporter_windows.go
@@ -0,0 +1,14 @@
+// +build windows
+
+package exporter
+
+import (
+	"io"
+	"net/http"
+)
+
+func NewHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "# /metrics is not implemented for windows platform")
+	})
+}
