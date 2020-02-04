c970f3a017cf0fa33a31b20e69c5983a0fce1bd0
navigaid <navigaid@gmail.com>
2020-02-04 15:13:34 +0800

    pkg/uuid: update demo with machineid

--- pkg/uuid/demo.go
+++ pkg/uuid/demo.go
@@ -4,11 +4,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/denisbrodbeck/machineid"
 	"k0s.io/k0s/pkg/uuid"
 )
 
 func main() {
+	mid, _ := machineid.ID()
+	log.Println(mid)
 	{
 		fmt.Println(uuid.New())
 		fmt.Println(uuid.New())
