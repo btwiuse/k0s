a43460e042ca2b46e0691de152f885d93f48dcfb
zouhangbiao <zouhangbiao@powerlaw.ai>
2020-12-21 20:38:28 +0800

    gitd: move impl to /pkg/gitd

--- pkg/cli/gitd/main.go
+++ pkg/cli/gitd/main.go
@@ -2,34 +2,18 @@ package gitd
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
-	"k0s.io/k0s/pkg/middleware"
 	"k0s.io/k0s/pkg/tunnel/listener"
+	"k0s.io/k0s/pkg/gitd"
 )
 
 func Run(args []string) (err error) {
 	var (
-		port   = args[0]
-		ln     = listener.Listener(port, "/")
-		gitMux = newGitMux()
+		port = args[0]
+		ln   = listener.Listener(port, "/")
 	)
 
 	log.Println("server is listening on", port)
 
-	return http.Serve(ln, middleware.LoggingMiddleware(gitMux))
-}
-
-func newGitMux() http.Handler {
-	r := mux.NewRouter()
-
-	// shamelessly borrowed from
-	// https://github.com/pratikju/servidor/blob/master/server.go#L28
-
-	r.HandleFunc(`/info/refs`, serviceHandler).Methods("GET")
-	r.HandleFunc(`/git-upload-pack`, uploadPackHandler).Methods("POST")
-	r.HandleFunc(`/git-receive-pack`, receivePackHandler).Methods("POST")
-
-	return r
+	return gitd.Serve(ln)
 }
---
+++ pkg/gitd/gitd.go
@@ -0,0 +1,31 @@
+package gitd
+
+import (
+	"net"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"k0s.io/k0s/pkg/middleware"
+)
+
+func Serve(ln net.Listener) error {
+	var (
+		gitMux  = newGitMux()
+		handler = middleware.LoggingMiddleware(gitMux)
+	)
+
+	return http.Serve(ln, handler)
+}
+
+func newGitMux() http.Handler {
+	r := mux.NewRouter()
+
+	// shamelessly borrowed from
+	// https://github.com/pratikju/servidor/blob/master/server.go#L28
+
+	r.HandleFunc(`/info/refs`, serviceHandler).Methods("GET")
+	r.HandleFunc(`/git-upload-pack`, uploadPackHandler).Methods("POST")
+	r.HandleFunc(`/git-receive-pack`, receivePackHandler).Methods("POST")
+
+	return r
+}
