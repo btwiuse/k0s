ee155dde0953eb823be613d1044342113ba47939
navigaid <navigaid@gmail.com>
2021-01-07 17:58:24 +0800

    raze: add serde example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate rand;
+extern crate serde;
 extern crate serde_json;
+
+use serde::{Serialize, Deserialize};
 
 fn print_rand() {
     println!("char: {}", rand::random::<char>());
@@ -23,8 +26,31 @@ fn print_json() {
     println!("{}", john.to_string());
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn print_serde() {
+    let point = Point { x: 1, y: 2 };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
+
+    // Convert the JSON string back to a Point.
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+}
+
 fn main() {
     println!("Hello, world!");
     print_rand::<>();
     print_json::<>();
+    print_serde::<>();
 }
