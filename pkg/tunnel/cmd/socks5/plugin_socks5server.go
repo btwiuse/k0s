f70018fb1d1616836fb67bb0b4960575f293e40e
navigaid <navigaid@gmail.com>
2022-07-24 06:19:56 +0800

    tunnel: support websocket payload
    add experimental portless socks5 server example

---
+++ pkg/tunnel/cmd/socks5/plugin_socks5server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/btwiuse/gost"
+	"k0s.io/pkg/tunnel/listener"
+)
+
+const HOST = "ws://127.0.0.1:8000/tmp/"
+
+var autoHandler = gost.AutoHandler()
+var teleportListener = listener.Listener(HOST, "/")
+var fsHandler = http.FileServer(http.Dir("/tmp/"))
+
+func autoServe(l net.Listener) {
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			continue
+		}
+
+		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Println("socks5server: recovered from panic:", r, c)
+				}
+			}()
+			autoHandler.Handle(c)
+			c.Close()
+		}()
+	}
+}
+
+func main() {
+	log.Println("listening on", HOST)
+	// http.Serve(teleportListener, fsHandler)
+	autoServe(teleportListener)
+}
