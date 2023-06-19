755960f1d76fc96c4c1d854ef4294f4c68f99bed
navigaid <navigaid@gmail.com>
2023-06-19 16:49:36 +0000

    fix reverseproxy redir error: unsupported protocol scheme ""
    https://tip.golang.org/doc/go1.20#reverseproxy_rewrite

--- pkg/reverseproxy/reverseproxy.go
+++ pkg/reverseproxy/reverseproxy.go
@@ -20,49 +20,10 @@ func Handler(addr string) http.Handler {
 		log.Fatalln(err)
 	}
 	rp := &httputil.ReverseProxy{
-		Director: func(r *http.Request) {
-			// log.Println("request as received by reverse proxy:")
-			// dumpRequest(r)
-
-			r.URL.Scheme = rpURL.Scheme
-			r.Host = rpURL.Host
-			r.URL.Host = r.Host
-
-			// log.Println("request after modification by reverse proxy:")
-			// dumpRequest(r)
-		},
-		ModifyResponse: func(r *http.Response) (err error) {
-			// log.Println("reverse proxy response status code:", r.StatusCode)
-			for r.StatusCode == 301 || r.StatusCode == 302 {
-				if err = resolve(r); err != nil {
-					return err
-				}
-			}
-			return nil
+		Rewrite: func(r *httputil.ProxyRequest) {
+			r.SetURL(rpURL)
+			r.SetXForwarded()
 		},
 	}
 	return rp
-}
-
-func resolve(r *http.Response) error {
-	resp, err := http.Get(r.Header.Get("Location"))
-	if err != nil {
-		return err
-	}
-	r.Status = resp.Status
-	r.StatusCode = resp.StatusCode
-	r.Proto = resp.Proto
-	r.ProtoMajor = resp.ProtoMajor
-	r.ProtoMinor = resp.ProtoMinor
-	r.Header = resp.Header
-	r.Body = resp.Body
-	r.ContentLength = resp.ContentLength
-	r.TransferEncoding = resp.TransferEncoding
-	r.Close = resp.Close
-	r.Uncompressed = resp.Uncompressed
-	r.Trailer = resp.Trailer
-	r.Request = resp.Request
-	r.TLS = resp.TLS
-	// log.Println("redirected reverse proxy response status code:", r.StatusCode)
-	return nil
 }
